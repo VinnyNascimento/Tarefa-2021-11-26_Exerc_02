79d613f57870f9cfee52e9f31cd6f0f688ea9ea5
VinnyNascimento <91098218+VinnyNascimento@users.noreply.github.com>
2021-12-02 13:56:56 -0300

    Add files via upload

---
+++ Exerc_02/Exerc_02/Calculo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exerc_02
+{
+    class Calculo
+    {
+        #region atributos
+        private int num1;
+        private int resultado;
+        #endregion
+
+        #region construtores
+        public Calculo()
+        {
+            this.num1 = 0;
+        }
+
+        public Calculo(int num1)
+        {
+            this.num1 = num1;
+        }
+        #endregion
+
+        #region métodos de interface (Setter's & Getter's)
+        public void setNum1(int num1)
+        {
+            this.num1 = num1;
+        }
+
+        public int getNum1()
+        {
+            return this.num1;
+        }
+
+        public int getResultado()
+        {
+            return this.resultado;
+        }
+        #endregion
+
+        #region métodos funcionais
+        public void calcular()
+        {
+            this.resultado = this.num1 * this.num1;
+        }
+        #endregion
+    }
+}
---
+++ Exerc_02/Exerc_02/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace Exerc_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAresta = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(116, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Descubra a Area de um Quadrado";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(269, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite o Valor da Aresta do Quadrado";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Área do Quadrado";
+            // 
+            // txtAresta
+            // 
+            this.txtAresta.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtAresta.Location = new System.Drawing.Point(301, 79);
+            this.txtAresta.Name = "txtAresta";
+            this.txtAresta.Size = new System.Drawing.Size(100, 27);
+            this.txtAresta.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCalcular.Location = new System.Drawing.Point(213, 135);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(107, 46);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.Location = new System.Drawing.Point(213, 217);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(26, 21);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "    ";
+            this.lblResultado.Click += new System.EventHandler(this.lblResultado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 299);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtAresta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Area de um Quadrado";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAresta;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
